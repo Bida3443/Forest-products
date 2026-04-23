20509433ed367d259bf971c3ef095cf073e06b32
Bida3443 <alexanderbamise@gmail.com>
2026-04-23 15:20:48 -0700

    forest products

--- src/components/Section3/Section3.tsx
+++ src/components/Section3/Section3.tsx
@@ -1,13 +1,18 @@
 
 import Image from "next/image"
 import Mini from "../../../public/chair2.png";
+import Minibook from "../../../public/Minibookshelf.png";
+import rocking from "../../../public/rocking.png";
+import tripod from "../../../public/tripodchair.png";
+import stool from "../../../public/carribean.png";
+import foam from "../../../public/foamchair.png"; 
 
 
 export default function OurProducts (){
     return( 
-        <section className="bg-white pt-20  h-screen ">
-            <div className="    overflow-hidden">
-                <div className="flex justify-between mb-10 px-25">
+        <section className="bg-white pt-20  min-h-screen ">
+            <div className="  overflow-hidden">
+                <div className="flex justify-between mb-10 lg:px-25 px-2">
                     <h1 className="text-black font-bold">OUR PRODUCTS</h1> 
                       <a
   href="/services"
@@ -25,12 +30,99 @@ export default function OurProducts (){
 </a>
                 </div>
 
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 px-25 md:px-6 lg:px-0">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 px-25 md:px-6 lg:px-25">
+  
+  <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-2">
+    
+    <Image
+      src={Minibook}
+      alt="Bookshelf"
+      className="w-full h-auto max-w-37.5 md:max-w-50 lg:max-w-62.5"
+    />
+
+    <h2 className="text-black text-sm md:text-base lg:text-lg text-center mt-2">
+      Mini Book Shelf
+    </h2>
+
+    <p className="text-black font-bold text-sm md:text-base">
+      ₦2,500
+    </p>
+
+    <button className="bg-[#F0BD09] text-white text-xs md:text-sm lg:text-[15px] px-8 md:px-10 py-1.5 rounded-xl mt-2 w-full md:w-auto">
+      Message to buy
+    </button>
+  </div>
+
+
+  <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
+    
+    <Image
+      src={Mini}
+      alt="Bookshelf"
+      className="w-full h-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px]"
+    />
+
+    <h2 className="text-black text-sm md:text-base lg:text-lg text-center mt-2">
+      Mini Book Shelf
+    </h2>
+
+    <p className="text-black font-bold text-sm md:text-base">
+      ₦2,500
+    </p>
+
+    <button className="bg-[#F0BD09] text-white text-xs md:text-sm lg:text-[15px] px-8 md:px-10 py-1.5 rounded-xl mt-2 w-full md:w-auto">
+      Message to buy
+    </button>
+  </div>
+
+
+  <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
+    
+    <Image
+      src={tripod}
+      alt="Bookshelf"
+      className="w-full h-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px]"
+    />
+
+    <h2 className="text-black text-sm md:text-base lg:text-lg text-center mt-2">
+      Mini Book Shelf
+    </h2>
+
+    <p className="text-black font-bold text-sm md:text-base">
+      ₦2,500
+    </p>
+
+    <button className="bg-[#F0BD09] text-white text-xs md:text-sm lg:text-[15px] px-8 md:px-10 py-1.5 rounded-xl mt-2 w-full md:w-auto">
+      Message to buy
+    </button>
+  </div>
+
+  <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
+    
+    <Image
+      src={stool}
+      alt="Bookshelf"
+      className="w-full h-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px]"
+    />
+
+    <h2 className="text-black text-sm md:text-base lg:text-lg text-center mt-2">
+      Mini Book Shelf
+    </h2>
+
+    <p className="text-black font-bold text-sm md:text-base">
+      ₦2,500
+    </p>
+
+    <button className="bg-[#F0BD09] text-white text-xs md:text-sm lg:text-[15px] px-8 md:px-10 py-1.5 rounded-xl mt-2 w-full md:w-auto">
+      Message to buy
+    </button>
+  </div>
+
   
   <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
     
     <Image
-      src={Mini}
+      src={rocking}
       alt="Bookshelf"
       className="w-full h-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px]"
     />
@@ -49,10 +141,10 @@ export default function OurProducts (){
   </div>
 
 
-  <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
+<div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
     
     <Image
-      src={Mini}
+      src={foam}
       alt="Bookshelf"
       className="w-full h-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px]"
     />
@@ -71,26 +163,6 @@ export default function OurProducts (){
   </div>
 
 
-  <div className="flex flex-col items-center justify-center rounded-xl border border-[#F0BD09] p-3 md:p-4">
-    
-    <Image
-      src={Mini}
-      alt="Bookshelf"
-      className="w-full h-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px]"
-    />
-
-    <h2 className="text-black text-sm md:text-base lg:text-lg text-center mt-2">
-      Mini Book Shelf
-    </h2>
-
-    <p className="text-black font-bold text-sm md:text-base">
-      ₦2,500
-    </p>
-
-    <button className="bg-[#F0BD09] text-white text-xs md:text-sm lg:text-[15px] px-8 md:px-10 py-1.5 rounded-xl mt-2 w-full md:w-auto">
-      Message to buy
-    </button>
-  </div>
 
   {/* Repeat for others */}
   
