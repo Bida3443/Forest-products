d859b4314492f2e1b7d566cc3f5957a22882bf65
Bida3443 <alexanderbamise@gmail.com>
2026-04-30 06:42:06 -0700

    forest products

--- src/components/Section4/Client.tsx
+++ src/components/Section4/Client.tsx
@@ -7,7 +7,7 @@ import Ellip from "../../../public/Ellipse 5.png";
 export default function Client () {
   return (
     <section className="bg-[#f3f3f3] py-20">
-        <div className="flex justify-center items-center flex-col">
+        <div className="flex justify-center items-center flex-col px-4">
                 <h1 className="text-black text-3xl font-bold">What our client says</h1>
             <div className="grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10 pt-10">
                 <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
@@ -15,17 +15,17 @@ export default function Client () {
                     <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
 
-                <div className="items-center bg-white justify-center lg:w-130 w-100  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
+                <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellip} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
                     <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
 
-                <div className="items-center bg-white justify-center lg:w-130 w-100  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
+                <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellips} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
                     <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
                 
-                <div className="items-center bg-white justify-center lg:w-130 w-100  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
+                <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellip} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
                     <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
