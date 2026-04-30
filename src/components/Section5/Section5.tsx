1abd1331c3b05da4072c6392ee935cea68dd63d3
Bida3443 <alexanderbamise@gmail.com>
2026-04-30 10:30:30 -0700

    forest products

--- src/components/Section5/Section5.tsx
+++ src/components/Section5/Section5.tsx
@@ -4,7 +4,7 @@ export default function Section ()  {
   return (
     <div className='bg-[#F0BD09]'>
         <div className='flex lg:flex-row flex-col justify-center lg:p-15 p-6 lg:gap-90 gap-5 items-center'>
-            <h1 className='lg:text-xl text-md'>Want to transform your space with our furniture?</h1>
+            <h1 className='lg:text-xl text-[8px] text-center'>Want to transform your space with our furniture?</h1>
             <button className='bg-[#4C4C4D] lg:py-3 py-2 lg:px-20 px-10 lg:text-xl text-md rounded-xl cursor-pointer transition-all duration-300'>Contact Us</button>
         </div>
     </div>
