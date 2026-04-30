c3f973231abffa6219a802e2a7a02ea6726fecff
Bida3443 <alexanderbamise@gmail.com>
2026-04-30 07:31:56 -0700

    forest products

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -12,7 +12,7 @@ export default function hero() {
   return (
     <section className="min-h-screen bg-[#F9F2DA] w-full ">
       <div className="flex flex-col lg:flex-row lg:pt-40 pt-10 space-y-10 justify-center lg:justify-between place-items-center text-black ">
-        <div className="pl-20 lg:items-start items-center">
+        <div className="lg:pl-20 pl-5 lg:items-start items-center">
           <div className="space-y-4   w-full lg:max-w-130">
             <h1 
             className="text-4xl font-bold text-[#F0BD09]">
