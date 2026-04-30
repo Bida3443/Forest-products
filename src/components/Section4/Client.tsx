bb78c808f9b304e18ee745dae0cc0f5d6372470c
Bida3443 <alexanderbamise@gmail.com>
2026-04-30 07:27:04 -0700

    forest products

--- src/components/Section4/Client.tsx
+++ src/components/Section4/Client.tsx
@@ -12,22 +12,22 @@ export default function Client () {
             <div className="grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10 pt-10">
                 <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellips} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
-                    <p className="text-black pl-15 w-100">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
+                    <p className="text-black pl-15 text-justify">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
 
                 <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellip} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
-                    <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
+                    <p className="text-black pl-15 text-justify">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
 
                 <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellips} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
-                    <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
+                    <p className="text-black pl-15 text-justify">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
                 
                 <div className="items-center bg-white justify-center w-full lg:max-w-130  p-4 rounded-xl border border-transparent hover:border-[#F0BD09] hover:shadow-md transition-all cursor-pointer duration-300 ">
                     <div className="flex items-center gap-5 "><Image src={Ellip} alt="Ellise Image" width={100} height={100} className="w-10 h-10"/><h2 className="text-black font-bold">Adewolu A.</h2></div>
-                    <p className="text-black pl-15">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
+                    <p className="text-black pl-15 text-justify">Best experience I have had with furniture designs in my home. Stress-free and great innovations.</p>
                 </div>
                 
             </div>
