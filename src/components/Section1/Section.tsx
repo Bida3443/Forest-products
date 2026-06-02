5721498abfddead67a43a2105ee1fb6efa4bc5f5
Bida3443 <alexanderbamise@gmail.com>
2026-06-02 15:03:14 -0700

    Forest

--- src/components/Section1/Section.tsx
+++ src/components/Section1/Section.tsx
@@ -1,88 +1,88 @@
 // import { div, section } from "motion/react-client";
 import Image from "next/image";
-import Theater from "../../../public/Theater.png";
-import Relaxation from "../../../public/Relaxation.png";
-import Office from "../../../public/Office.png";
-import Kitchen from "../../../public/Kitchen.png";
-import Living from "../../../public/Living Room.png";
-import Bedroom from "../../../public/Bed Room.png";
+import Theater from "../../../public/Theater.jpg";
+import Relaxation from "../../../public/Relaxation.jpg";
+import Office from "../../../public/Office.jpg";
+import Kitchen from "../../../public/Kitchen.jpg";
+import Living from "../../../public/Living Room.jpg";
+import Bedroom from "../../../public/Bed Room.jpg";
 
 export default function Services() {
   return (
-    <section className="bg-white h-auto lg:min-h-screen w-full">
+    <section className="bg-white h-auto min-h-screen w-full">
       <div className="flex flex-col place-items-center gap-10">
         <h1 className="text-black text-3xl font-bold">Our Services</h1>
         <div className="grid grid-cols-1 items-center justify-center lg:px-0 px-5 text-black">
           <div className="grid lg:grid-cols-3 grid-cols-2 gap-10">
-            <div className="relative w-full   group overflow-hidden ">
+            <div className="relative w-full rounded-xl   group overflow-hidden ">
               <Image
                 src={Theater}
                 alt="Rectangle"
                 width={300}
                 height={300}
-                className="w-full h-auto   transition-transform duration-500 group-hover:scale-110"
+                className="w-full h-auto rounded-xl   transition-transform duration-500 group-hover:scale-110"
               />{" "}
               <p className="absolute  bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
                 Lecture Theater Funiture
               </p>
             </div>
-            <div className="relative w-full">
+            <div className="relative w-full rounded-xl   group overflow-hidden">
               <Image
                 src={Relaxation}
                 alt="Rectangle"
                 width={300}
                 height={300}
-                className="w-full h-auto"
+                className="w-full h-auto rounded-xl   transition-transform duration-500 group-hover:scale-110"
               />{" "}
-              <p className="absolute inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
+              <p className="absolute  bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
                 Relaxaton Funiture
               </p>
             </div>
-            <div className="relative w-full">
+            <div className="relative w-full rounded-xl   group overflow-hidden">
               <Image
                 src={Office}
                 alt="Rectangle"
                 width={300}
                 height={300}
-                className="w-full h-auto"
+                className="w-full h-auto rounded-xl   transition-transform duration-500 group-hover:scale-110"
               />{" "}
-              <p className="absolute inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
+              <p className="absolute  bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
                 Office Funiture
               </p>
             </div>
-            <div className="relative w-full">
+            <div className="relative w-full rounded-xl   group overflow-hidden">
               <Image
                 src={Kitchen}
                 alt="Rectangle"
                 width={300}
                 height={300}
-                className="w-full h-auto"
+                className="w-full h-auto rounded-xl   transition-transform duration-500 group-hover:scale-110"
               />{" "}
-              <p className="absolute inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
+              <p className="absolute  bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
                 Kitchen Funiture
               </p>
             </div>
-            <div className="relative w-full">
+            <div className="relative w-full rounded-xl   group overflow-hidden">
               <Image
                 src={Living}
                 alt="Rectangle"
                 width={300}
                 height={300}
-                className="w-full h-auto"
+                className="w-full h-auto rounded-xl   transition-transform duration-500 group-hover:scale-110"
               />{" "}
-              <p className="absolute inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
+              <p className="absolute  bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
                 Living Room Funiture
               </p>
             </div>
-            <div className="relative w-full">
+            <div className="relative w-full rounded-xl   group overflow-hidden">
               <Image
                 src={Bedroom}
                 alt="Rectangle"
                 width={300}
                 height={300}
-                className="w-full h-auto"
+                className="w-full h-auto rounded-xl   transition-transform duration-500 group-hover:scale-110"
               />{" "}
-              <p className="absolute inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
+              <p className="absolute  bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 inset-0 flex items-center justify-center text-white text-sm md:text-lg font-semibold text-center px-2">
                 Bed Room Funiture
               </p>
             </div>
