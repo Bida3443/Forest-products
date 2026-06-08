733e28fc0b411e13f9f893c377b1fed6dbcc4663
Bida3443 <alexanderbamise@gmail.com>
2026-06-07 19:54:47 -0700

    Forest

--- app/about/hero.tsx
+++ app/about/hero.tsx
@@ -15,7 +15,7 @@ export default function hero() {
                 <div className="absolute inset-0 w-full h-full bg-black opacity-60 z-20" />
                 <div className="absolute inset-0 z-30 flex flex-col justify-center items-center text-white px-4">
                     <h1 className="lg:text-4xl text-[20px] text-[#F0BD09] font-bold mb-3">About Us</h1>
-                    <p className=" max-w-2xl text-center">Welcome to our  Forest Product Development and Utilization website! We are your go-to source for stylish, high-quality furniture pieces that will transform your space. From comfortable sofas to elegant dining tables, our collection has everything you need to create the home of your dreams. Browse our website now and discover a world of possibilities for your living space.</p>
+                    <p className=" lg:max-w-2xl max-w-80.5 lg:text-[15px] text-[10px] text-center">Welcome to our  Forest Product Development and Utilization website! We are your go-to source for stylish, high-quality furniture pieces that will transform your space. From comfortable sofas to elegant dining tables, our collection has everything you need to create the home of your dreams. Browse our website now and discover a world of possibilities for your living space.</p>
 
                 </div>
             </div>
