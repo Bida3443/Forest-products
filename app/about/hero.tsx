f4a21209aa6a4469c5daeb33169e36db251d33a5
Bida3443 <alexanderbamise@gmail.com>
2026-06-08 12:16:35 -0700

    Forest

--- app/about/hero.tsx
+++ app/about/hero.tsx
@@ -10,9 +10,9 @@ export default function hero() {
                     alt="background image"
                     width={500}
                     height={500}
-                    style={{ objectFit: "cover" }}
-                    className="w-full h-full" />
-                <div className="absolute inset-0 w-full h-full bg-black opacity-60 z-20" />
+                    style={{ objectFit: "cover"}}
+                    className="w-full h-full"/>
+                <div className="absolute inset-0 w-full h-full bg-black opacity-70 z-20" />
                 <div className="absolute inset-0 z-30 flex flex-col justify-center items-center text-white px-4">
                     <h1 className="lg:text-4xl text-[20px] text-[#F0BD09] font-bold mb-3">About Us</h1>
                     <p className=" lg:max-w-2xl max-w-80.5 lg:text-[15px] text-[10px] text-center">Welcome to our  Forest Product Development and Utilization website! We are your go-to source for stylish, high-quality furniture pieces that will transform your space. From comfortable sofas to elegant dining tables, our collection has everything you need to create the home of your dreams. Browse our website now and discover a world of possibilities for your living space.</p>
--- app/about/sectionabout.tsx
+++ app/about/sectionabout.tsx
@@ -1,16 +1,54 @@
 
+import Image from "next/image"
+import ellipse from "../../public/Ellipse 7.png"
+import ellipes from "../../public/ellipse2.jpg"
 
-export default function serviceabout () {
-    return(
+export default function serviceabout() {
+    return (
         <section className="bg-white lg:py-30 py-5  overflow-hidden">
-            <div className="flex flex-row justify-between px-35 items-center">
-                <div className="space-x-4">
-                    <h1 className="text-3xl font-bold">Who we are</h1>
-                    <p className="lg:max-w-s max-w-90.5 lg:text-[14px] text-justify text-[10px]">Forest Products Development and Utilization Department (FPD&U) is one of the four (4) departments in the Forestry Research Institute of Nigeria (FRIN) which was established in 1954. The Institute's Decree 35 of 1973 and other establishing Research Institute of 1977 changed the status of the Department to an institute being supervised by the Federal Ministry of Environment. The department has identified and is still identifying properties of various wood species for proper usage. Research work on different forest fruits and bamboo and rattan as non-timber forest products has witnessed tremendous achievement in term of food production and wealth creation.</p>
+            <div className="flex lg:flex-row flex-col justify-between px-35 items-center">
+                <div className="space-y-4">
+                    <h1 className="lg:text-3xl text-2xl font-bold">Who we are</h1>
+                    <p className="lg:max-w-s lg:max-w-95.5 max-w-150 lg:text-[14px] text-justify text-[10px]">Forest Products Development and Utilization Department (FPD&U) is one of the four (4) departments in the Forestry Research Institute of Nigeria (FRIN) which was established in 1954. The Institute's Decree 35 of 1973 and other establishing Research Institute of 1977 changed the status of the Department to an institute being supervised by the Federal Ministry of Environment. The department has identified and is still identifying properties of various wood species for proper usage. Research work on different forest fruits and bamboo and rattan as non-timber forest products has witnessed tremendous achievement in term of food production and wealth creation.</p>
                 </div>
-                <div>dddddddddd</div>
+                {/* <div className="relative z-10">
+                    <div className="">
+                        <Image src={ellipse} alt="" width={300} height={300} className=""/>
+                    </div>
+                    <div className="">
+                        <Image src={ellipes} alt="" width={300} height={300} className="absolute top-10 right-0"/>
+                    </div>
+                </div> */}
+
+                <div className="relative z-10 w-100 h-112.5">
+
+                    {/* Yellow Shape */}
+                    <Image
+                        src={ellipse}
+                        alt=""
+                        fill
+                        className="object-contain"
+                    />
+
+                    {/* Photo */}
+                    <div
+                        className="absolute top-0 left-0 w-full h-full overflow-hidden"
+                        style={{
+                            clipPath:
+                                "ellipse(45% 45% at 50% 50%)"
+                        }}
+                    >
+                        <Image
+                            src={ellipes}
+                            alt=""
+                            fill
+                            className="object-cover "
+                        />
+                    </div>
+
+                </div>
             </div>
-            
+
         </section>
     )
 }
