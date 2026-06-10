e26335c48ec0ba9e5171bc0ed0635c80da3b0131
Bida3443 <alexanderbamise@gmail.com>
2026-06-09 18:00:31 -0700

    Forest

--- app/about/section5about.tsx
+++ app/about/section5about.tsx
@@ -8,8 +8,8 @@ import rec3 from "../../public/rec3.png"
 
 export default function section5about() {
     return (
-        <section className="bg- min-h-screen overflow-hidden">
-          <div className="flex flex-col justify-center space-y-20 items-center">
+        <section className="bg- min-h-screen bg-white overflow-hidden">
+          <div className="flex py-10 flex-col justify-center space-y-20 items-center">
             <div className="flex flex-col max-w-max justify-center items-center">
                  <h1 className="lg:text-3xl text-[20px] text-black font-bold mb-3">Meet Our Professionals</h1>
                     <p className="lg:max-w-2xl max-w-70.5 lg:text-[15px] text-[10px]  justify-center text-center">We have a team of dedicated professionals who are experts in the furniture industry and are committed to helping you find the perfect pieces for your home or office</p>
@@ -17,17 +17,33 @@ export default function section5about() {
             <div className="grid lg:grid-cols-3 grid-cols-2 lg:gap-30 gap-10 ">
                 <div className="flex flex-col justify-center items-center ">
                     <Image src={rec1} alt="" width={200} height={200}/>
-                    <h1 className="text-[15px] font-bold">Dr. Areo Olusola</h1>
+                    <h1 className="text-[15px] pt-3 font-bold">Dr. Areo Olusola</h1>
                     <p className="text-[10px]">Head of Department</p>
                 </div>
                 <div className="flex flex-col justify-center items-center">
                     <Image src={rec2} alt="" width={200} height={200}/>
-                    <h1 className="text-[15px] font-bold">Mr. Adeniyi Segun</h1>
+                    <h1 className="text-[15px] pt-3 font-bold">Mr. Adeniyi Segun</h1>
                     <p className="text-[10px]">Head of Wood Anatomy Section</p>
                 </div>
                 <div className="flex flex-col justify-center items-center">
                     <Image src={rec3} alt="" width={200} height={200}/>
-                    <h1 className="text-[15px] font-bold">Dr. Amoo-Onidundu</h1>
+                    <h1 className="text-[15px] pt-3 font-bold">Dr. Amoo-Onidundu</h1>
+                    <p className="text-[10px]">Head of Wood Seasoning Section</p>
+                </div>
+
+                <div className="flex flex-col justify-center items-center ">
+                    <Image src={rec1} alt="" width={200} height={200}/>
+                    <h1 className="text-[15px] pt-3 font-bold">Dr. Areo Olusola</h1>
+                    <p className="text-[10px]">Head of Department</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <Image src={rec2} alt="" width={200} height={200}/>
+                    <h1 className="text-[15px] pt-3 font-bold">Mr. Adeniyi Segun</h1>
+                    <p className="text-[10px]">Head of Wood Anatomy Section</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <Image src={rec3} alt="" width={200} height={200}/>
+                    <h1 className="text-[15px] pt-3 font-bold">Dr. Amoo-Onidundu</h1>
                     <p className="text-[10px]">Head of Wood Seasoning Section</p>
                 </div>
             </div>
