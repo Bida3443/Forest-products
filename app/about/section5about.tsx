cefae217330b813e1b003b3871c23343aa595fdd
Bida3443 <alexanderbamise@gmail.com>
2026-06-16 14:25:38 -0700

    Forest

--- app/about/section5about.tsx
+++ app/about/section5about.tsx
@@ -1,3 +1,4 @@
+"use client"
 
 import Image from "next/image"
 import rec1 from "../../public/rec1.png"
@@ -16,98 +17,189 @@ import rec13 from "../../public/rec13.png"
 import rec14 from "../../public/rec14.png"
 import rec15 from "../../public/rec15.png"
 
+import { motion } from "framer-motion"
 
 export default function section5about() {
     return (
         <section className="bg- min-h-screen bg-white overflow-hidden">
-          <div className="flex pb-15 flex-col justify-center space-y-10 items-center">
-            <div className="flex flex-col max-w-max justify-center items-center">
-                 <h1 className="lg:text-3xl text-[20px] text-[#4C4C4D] font-bold mb-3">Meet Our Professionals</h1>
+            <div className="flex pb-15 flex-col justify-center space-y-10 items-center">
+                <div className="flex flex-col max-w-max justify-center items-center">
+                    <h1 className="lg:text-3xl text-[20px] text-[#4C4C4D] font-bold mb-3">Meet Our Professionals</h1>
                     <p className="lg:max-w-2xl max-w-70.5 lg:text-[15px] text-[10px] text-black justify-center text-center">We have a team of dedicated professionals who are experts in the furniture industry and are committed to helping you find the perfect pieces for your home or office</p>
+                </div>
+                <div className="grid lg:grid-cols-3 grid-cols-2 lg:gap-30 lg:px-0 px-6 gap-10 ">
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+
+                        className="flex flex-col justify-center items-center ">
+                        <Image src={rec1} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Areo Olusola</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black ">Head of Department</p>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+
+                        className="flex flex-col justify-center items-center">
+                        <Image src={rec2} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px]  text-black pt-3 font-bold">Mr. Adeniyi Segun</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Anatomy Section</p>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+
+                        className="flex flex-col justify-center items-center">
+                        <Image src={rec3} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Amoo-Onidundu</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Seasoning Section</p>
+                    </motion.div>
+
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+
+                        className="flex flex-col justify-center items-center ">
+                        <Image src={rec4} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Ebele Jonathan</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Mechanics Section</p>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+
+                        className="flex flex-col justify-center items-center">
+                        <Image src={rec5} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Adiji Ayodeji</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Preservation Section</p>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+
+                        className="flex flex-col justify-center items-center">
+                        <Image src={rec6} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Adedamola Adio</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Processing Section</p>
+                    </motion.div>
+
+                    <motion.div
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center ">
+                        <Image src={rec7} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Aliu Babalola</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black ">Head of Wood Waste Utilization Section</p>
+                    </motion.div>
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center">
+                        <Image src={rec8} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px]  text-black pt-3 font-bold">Mr. Felix Adesope</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Bamboo and Rattan Section</p>
+                    </motion.div>
+                    <motion.div
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center">
+                        <Image src={rec9} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Kazeem Suleiman</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Seasoning Section</p>
+                    </motion.div>
+
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center ">
+                        <Image src={rec10} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Chibuzor Akpan</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Bio-energy Section</p>
+                    </motion.div>
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center">
+                        <Image src={rec11} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Miss Michelle Davidson</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Chemistry Section</p>
+                    </motion.div>
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center">
+                        <Image src={rec12} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Adubi Olajumoke</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Wood Libarian</p>
+                    </motion.div>
+
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center ">
+                        <Image src={rec13} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Amanda Obi</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Production Manager</p>
+                    </motion.div>
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center">
+                        <Image src={rec14} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Akanni Sunday</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Chief Craftsman</p>
+                    </motion.div>
+                    <motion.div 
+                    initial={{ y: 200, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    
+                    className="flex flex-col justify-center items-center">
+                        <Image src={rec15} alt="" width={200} height={200} />
+                        <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Jumoke Ayoola</h1>
+                        <p className="lg:text-[10px] text-[6px] text-black">Marketing Manager</p>
+                    </motion.div>
+
+                </div>
             </div>
-            <div className="grid lg:grid-cols-3 grid-cols-2 lg:gap-30 lg:px-0 px-6 gap-10 ">
-                <div className="flex flex-col justify-center items-center ">
-                    <Image src={rec1} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Areo Olusola</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black ">Head of Department</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec2} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px]  text-black pt-3 font-bold">Mr. Adeniyi Segun</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Anatomy Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec3} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Amoo-Onidundu</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Seasoning Section</p>
-                </div>
-
-                <div className="flex flex-col justify-center items-center ">
-                    <Image src={rec4} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Ebele Jonathan</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Mechanics Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec5} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Adiji Ayodeji</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Preservation Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec6} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Adedamola Adio</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Processing Section</p>
-                </div>
-
-                <div className="flex flex-col justify-center items-center ">
-                    <Image src={rec7} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Aliu Babalola</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black ">Head of Wood Waste Utilization Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec8} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px]  text-black pt-3 font-bold">Mr. Felix Adesope</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Bamboo and Rattan Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec9} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Kazeem Suleiman</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Seasoning Section</p>
-                </div>
-
-                <div className="flex flex-col justify-center items-center ">
-                    <Image src={rec10} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Dr. Chibuzor Akpan</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Bio-energy Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec11} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Miss Michelle Davidson</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Head of Wood Chemistry Section</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec12} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Adubi Olajumoke</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Wood Libarian</p>
-                </div>
-
-                <div className="flex flex-col justify-center items-center ">
-                    <Image src={rec13} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Amanda Obi</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Production Manager</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec14} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mr. Akanni Sunday</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Chief Craftsman</p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Image src={rec15} alt="" width={200} height={200}/>
-                    <h1 className="lg:text-[15px] text-[10px] text-black pt-3 font-bold">Mrs. Jumoke Ayoola</h1>
-                    <p className="lg:text-[10px] text-[6px] text-black">Marketing Manager</p>
-                </div>
-
-            </div>
-          </div>
         </section>
     )
 }
