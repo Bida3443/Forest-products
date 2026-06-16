8c43642af5f281312b78cdfdf9b1dca08dcaa4e0
Bida3443 <alexanderbamise@gmail.com>
2026-06-16 14:10:32 -0700

    Forest

--- app/about/hero.tsx
+++ app/about/hero.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Image from "next/image"
 import About from "../../public/about.jpg"
+import { motion } from "framer-motion";
 
 export default function hero() {
     return (
@@ -14,8 +17,18 @@ export default function hero() {
                     className="w-full h-full"/>
                 <div className="absolute inset-0 w-full h-full bg-black opacity-70 z-20" />
                 <div className="absolute inset-0 z-30 flex flex-col justify-center items-center text-white px-4">
-                    <h1 className="lg:text-4xl text-[20px] text-[#F0BD09] font-bold mb-3">About Us</h1>
-                    <p className=" lg:max-w-2xl max-w-80.5 lg:text-[15px] text-[10px] text-center">Welcome to our  Forest Product Development and Utilization website! We are your go-to source for stylish, high-quality furniture pieces that will transform your space. From comfortable sofas to elegant dining tables, our collection has everything you need to create the home of your dreams. Browse our website now and discover a world of possibilities for your living space.</p>
+                    <motion.h1 
+                    initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 2 }}
+                    
+                    className="lg:text-4xl text-[20px] text-[#F0BD09] font-bold mb-3">About Us</motion.h1>
+                    <motion.p 
+                    initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 4.4 }}
+
+                    className=" lg:max-w-2xl max-w-80.5 lg:text-[15px] text-[10px] text-center">Welcome to our  Forest Product Development and Utilization website! We are your go-to source for stylish, high-quality furniture pieces that will transform your space. From comfortable sofas to elegant dining tables, our collection has everything you need to create the home of your dreams. Browse our website now and discover a world of possibilities for your living space.</motion.p>
 
                 </div>
             </div>
--- app/about/sectionabout.tsx
+++ app/about/sectionabout.tsx
@@ -1,14 +1,20 @@
+"use client"
 
 import Image from "next/image"
 import ellipse from "../../public/Ellipse 7.png"
 import ellipes from "../../public/ellipse2.jpg"
+import { motion } from "framer-motion"
+
 
 export default function serviceabout() {
     return (
         <section className="bg-white lg:py-10 py-5 overflow-hidden">
             <div className="flex lg:flex-row flex-col justify-between lg:px-35 px-5 items-center">
                 <div className="space-y-4">
-                    <h1 className="lg:text-3xl text-[#4C4C4D]  text-2xl font-bold">Who we are</h1>
+                    <motion.h1 initial={{ opacity: 0, x: -30 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 2 }}
+                        className="lg:text-3xl text-[#4C4C4D]  text-2xl font-bold">Who we are</motion.h1>
                     <p className="lg:max-w-s lg:max-w-125.5 z-0 max-w-110 lg:text-[14px] text-[#B6B6B7] text-justify text-[10px]">Forest Products Development and Utilization Department (FPD&U) is one of the four (4) departments in the Forestry Research Institute of Nigeria (FRIN) which was established in 1954. The Institute's Decree 35 of 1973 and other establishing Research Institute of 1977 changed the status of the Department to an institute being supervised by the Federal Ministry of Environment. The department has identified and is still identifying properties of various wood species for proper usage. Research work on different forest fruits and bamboo and rattan as non-timber forest products has witnessed tremendous achievement in term of food production and wealth creation.</p>
                 </div>
                 <div className="relative z-10 lg:w-100 w-60 lg:h-112.5 h-70">
@@ -22,7 +28,10 @@ export default function serviceabout() {
                     />
 
                     {/* Photo */}
-                    <div
+                    <motion.div
+                    initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 2 }}
                         className="absolute top-0 left-0 w-full h-full overflow-hidden"
                         style={{
                             clipPath:
@@ -35,7 +44,7 @@ export default function serviceabout() {
                             fill
                             className="object-cover "
                         />
-                    </div>
+                    </motion.div>
 
                 </div>
             </div>
